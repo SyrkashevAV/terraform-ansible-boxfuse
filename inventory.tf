2018ea4a00d42b6aec77e14c9690cae256ee37bd
samalyshev <samalyshev@sberbank.ru>
2021-11-01 17:27:47 +0300

    test

--- inventory.tf
+++ inventory.tf
@@ -1,7 +1,7 @@
 resource "local_file" "ansible_inventory" {
-  content = templatefile("inventory/inventory.tmpl", {
+  content = templatefile("inventory.tmpl", {
     build_ip = yandex_compute_instance.build.network_interface[0].nat_ip_address,
     prod_ip  = yandex_compute_instance.prod.network_interface[0].nat_ip_address
   })
-  filename = format("%s/%s", abspath(path.root), "inventory/inventory.yaml")
+  filename = format("%s/%s", abspath(path.root), "inventory.yaml")
 }
