e477f17b05da039ab11681e86f369cea6ac48bd0
samalyshev <samalyshev@sberbank.ru>
2021-11-03 09:42:44 +0300

    add backend terraform

--- terraform/create-instance.tf
+++ terraform/create-instance.tf
@@ -1,3 +1,19 @@
+variable "yandex-token" {}
+
+variable "yandex-cloud-id" {}
+
+variable "yandex-folder-id" {}
+
+variable "yandex-zone" {}
+
+variable "terraform-backend-bucket" {}
+
+variable "backend-key" {}
+
+variable "access-key" {}
+
+variable "secret_key" {}
+
 terraform {
   required_providers {
     yandex = {
