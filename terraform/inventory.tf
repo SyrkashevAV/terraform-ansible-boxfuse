8c3281f1a076855cc6169be8910c53538ef63c1a
SyrkashevAV <syrkashev-av@yandex.ru>
2024-03-22 23:06:36 +0300

    Change3

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -3,4 +3,5 @@ resource "local_file" "ansible_inventory" {
     build_ip = yandex_compute_instance.build[0].network_interface[0].nat_ip_address,
     prod_ip  = yandex_compute_instance.build[1].network_interface[0].nat_ip_address
   })
+  filename = "./../ansible/inventory/inventory.yaml"
 }
