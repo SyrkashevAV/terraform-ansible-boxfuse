0b9229e28a7905199fc583c7549d33eaf663ea46
SyrkashevAV <syrkashev-av@yandex.ru>
2024-03-23 10:28:22 +0300

    Change3

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -54,6 +54,10 @@ resource "yandex_compute_instance" "build" {
     }
   }
 
+  metadata = {
+    ssh-keys = "extor:${file(var.public_key_path)}"
+  }
+
 
   provisioner "remote-exec" {
     inline = ["echo Connected!"]
