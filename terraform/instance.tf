4e232bf9b415ff5bfb480ddf74d6e8504603c535
SyrkashevAV <syrkashev-av@yandex.ru>
2024-03-27 21:24:31 +0300

    Change7

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -23,7 +23,7 @@ resource "yandex_vpc_network" "network" {
 }
 
 resource "yandex_vpc_subnet" "subnet" {
-  name = "subnet1"
+  name = "subnet2"
   zone = "${var.zone}"
   network_id = yandex_vpc_network.network.id
   v4_cidr_blocks = ["10.0.0.0/24"]
